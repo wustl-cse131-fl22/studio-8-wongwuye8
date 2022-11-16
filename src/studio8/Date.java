4afc29adeb9915368cd87092221d79f7ac2346f5
austinye <austinye@localhost>
2022-11-16 16:23:01 -0600

    ok

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,64 @@
 package studio8;
 
 public class Date {
+	
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	/**
+	 * Makes the date
+	 * @param m month
+	 * @param d day
+	 * @param y year
+	 */
+	public Date (int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+	}
+	
+	public int getMonth() {
+		return month;
+	}
 
+	public void setMonth(int month) {
+		this.month = month;
+	}
 
-    public static void main(String[] args) {
+	public int getDay() {
+		return day;
+	}
 
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isHoliday() {
+		return holiday;
+	}
+
+	public void setHoliday(boolean holiday) {
+		this.holiday = holiday;
+	}
+	
+	
+@Override
+	public String toString() {
+		return month + "/" + day + "/" + year;
+}    
+public static void main(String[] args) {
+		Date today = new Date (11, 16, 2022);
+    	System.out.println("the date is: " + today.toString()); 
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -3,8 +3,63 @@ package studio8;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean military;
+	/**
+	 * Makes the time
+	 * @param h hour
+	 * @param m minute
+	 */
+	public Time (int h, int m, boolean mil) {
+		hour = h;
+		minute = m;
+		military = mil;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+
+	public int getMinute() {
+		return minute;
+	}
+
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+
+	public boolean isMilitary() {
+		return military;
+	}
+
+
+	public void setMilitary(boolean military) {
+		this.military = military;
+	}
+
+
+	public String toString() {
+		if(military) {
+			return hour + ":" + minute;
+		}
+		else {
+			return (hour-12) + ":" + minute;
+		}
+		}
+	
+	
 	public static void main(String[] args) {
- 
+		Time now = new Time (16, 22, false);
+    	System.out.println("the time is: " + now.toString()); 
     	
     }
 
